d058388028626b52ec313789526250aea368d94e
sergeysirenko <s.sirenko88@gmail.com>
2022-05-14 14:17:38 +0300

    #24 16:30

---
+++ src/components/AddTicker.vue
@@ -0,0 +1,92 @@
+<template>
+    <section>
+        <div class="flex">
+            <div class="max-w-xs">
+                <label for="wallet" class="block text-sm font-medium text-gray-700">Тикер {{ ticker }}</label>
+                <div class="mt-1 relative rounded-md shadow-md">
+                    <input
+                        v-model="ticker"
+                        @keydown.enter="add(ticker)"
+                        @keyup="find"
+                        @input="isAddedTicker = false"
+                        type="text"
+                        name="wallet"
+                        id="wallet"
+                        class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
+                        placeholder="Например DOGE"
+                    />
+                </div>
+                <div v-if="ticker.length" class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap">
+                        <span
+                            v-for="coin in coinsList"
+                            :key="coin.key"
+                            @click="add(coin)"
+                            class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer"
+                        >
+                            {{ coin }}
+                        </span>
+                </div>
+                <div v-if="isAddedTicker" class="text-sm text-red-600">Такой тикер уже добавлен</div>
+            </div>
+        </div>
+        <AddButton class="my-4" @click="add(ticker)" />
+    </section>
+</template>
+
+<script>
+import AddButton from '@/components/AddButton';
+
+export default {
+    name: "AddTicker",
+    components: {
+        AddButton,
+    },
+
+    props: {
+        coinsList: {
+            type: Array,
+            default: () => [],
+        },
+
+        tickersList: {
+            type: Array,
+            default: () => [],
+        },
+    },
+
+    data() {
+        return {
+            ticker: '',
+            isAddedTicker: false,
+        }
+    },
+
+    methods: {
+        add(coin) {
+            if(this.checkCoin(coin)) return;
+
+            this.$emit('add-ticker', coin);
+        },
+
+        checkCoin(coin) {
+            this.isAddedTicker = false;
+
+            this.tickersList.forEach((ticker) => {
+                if (ticker.name === coin) {
+                    this.isAddedTicker = true;
+                }
+            });
+
+            return this.isAddedTicker;
+        },
+
+        find() {
+            this.$emit('find-ticker', this.ticker)
+        },
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
